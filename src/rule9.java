e2290e848b0eb9838f26ad9f67713af9df8e5d8c
laielvin <elvin_lai@hotmail.com>
2015-11-25 19:45:06 -0800

    Create rule9.java

---
+++ src/rule9.java
@@ -0,0 +1,9 @@
+import java.util.*; 
+
+public class rule9 {
+
+	public static void main(String[] args) throws IOException {
+	
+	//Check each word for any letter character repeated three times back to back and if there is return a true value for rule 9
+	
+	
